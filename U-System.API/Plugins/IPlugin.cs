adc616ff97e8e156f70f86fd508b3b7b81ea7d48
Hugo Marques <51680761+WinterStudios@users.noreply.github.com>
2020-12-21 10:36:16 +0000

    dev.1

---
+++ U-System.API/Plugins/IPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace U_System.API.Plugins
+{
+    public interface IPlugin
+    {
+        /// <summary>
+        /// Name of the Plugin
+        /// </summary>
+        string Name { get; }
+        string Discreption { get; }
+        SemVersion Version { get; }
+    }
+}
