2199ac1d6124e2a805cd4f08d732f8d3de84025e
Hugo Marques <51680761+WinterStudios@users.noreply.github.com>
2021-05-04 21:29:57 +0100

    v20.05.04

--- U-System.External/Core/Settings.cs
+++ U-System.External/Core/Settings.cs
@@ -6,6 +6,6 @@ namespace U_System.External.Core
 {
     public class Settings
     {
-        public static string _GITHUB_ACCESS_TOKEN { get => "c74b9498d671d4777ddccdbc7d3f85937a191fbd"; }
+        public static string _GITHUB_ACCESS_TOKEN { get => "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"; }
     }
 }
