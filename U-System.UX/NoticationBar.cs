c899a33597c53ae6295704fedbe39fecb529097d
Hugo Marques <51680761+WinterStudios@users.noreply.github.com>
2021-05-06 21:36:52 +0100

    v.21.05.06

---
+++ U-System.UX/NoticationBar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace U_System.UX
+{
+    public class NotificationBar
+    {
+        public static StatusBar StatusBar { get; private set; }
+
+        public static void Inicialize()
+        {
+
+        }
+    }
+}
