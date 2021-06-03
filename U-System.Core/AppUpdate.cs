10990a7a08f478530ac998b631cb473b1427e298
Hugo Marques <51680761+WinterStudios@users.noreply.github.com>
2021-06-03 12:29:40 +0100

    Create AppUpdate.cs

---
+++ U-System.Core/AppUpdate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U_System.Core
+{
+    internal class AppUpdate
+    {
+        public static string Version { get; set}
+        public static void Init()
+        {
+
+        }
+    }
+}
