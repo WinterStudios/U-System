59fd4c16e577b24c2fdb207fdb6b6879f73ee225
Hugo Marques <51680761+WinterStudios@users.noreply.github.com>
2021-06-22 23:05:26 +0100

    Create ThemeSystem.cs

---
+++ U-System.UX/ThemeSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U_System.UX
+{
+    public class ThemeSystem
+    {
+        public static void Initialize()
+        {
+
+        }
+    }
+}
